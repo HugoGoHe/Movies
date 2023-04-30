f3d05dbb22fffebb91e20d4e80b72b1cde11e589
HugoGoHe <A01640856@itesm.mx>
2023-04-30 00:00:29 -0600

    La pagina ya muestra las peliculas mas populares y muestra modal con info al clickear el boton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import MovieBox from './MovieBox';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=ba16880dd0b146d3f4fa4421c6cd688d";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  
+  const fetchData = () => {
+    fetch(API_URL)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    //  console.log(data.results);
+
+      setMovies(data.results);
+      //console.log(); // add this line to see what data is being returned
+    })
+    .catch((err) => {
+      console.error(err)
+    });
+  };
+
+  //Se usa useEffect para hacer la peticion a la API
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <div className='container'>
+      <div className='grid'>
+        {Array.prototype.map.call(movies, (movieReq)=> 
+        <MovieBox key={movieReq.id} {...movieReq}/>)}
+      </div>
+    </div>    
   );
 }
+
 
 export default App;
